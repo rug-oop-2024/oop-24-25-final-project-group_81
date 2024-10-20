5578b28d9f9bb1c809f476517897d48c509444a4
Petar <ppaureus@gmail.com>
2024-10-20 15:12:16 +0200

    updated the model __init__

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,13 +1,20 @@
 
 from autoop.core.ml.model.model import Model
-from autoop.core.ml.model.regression import MultipleLinearRegression
+from autoop.core.ml.model.regression.multiple_linear_regression import MultipleLinearRegression
 
 REGRESSION_MODELS = [
+    "MultipleLinearRegression"
 ] # add your models as str here
 
 CLASSIFICATION_MODELS = [
+    "K_nearest_neighbour"
 ] # add your models as str here
 
 def get_model(model_name: str) -> Model:
-    """Factory function to get a model by name."""
-    raise NotImplementedError("To be implemented.")
+    if model_name not in REGRESSION_MODELS or CLASSIFICATION_MODELS:
+        print(f"No such model `{model_name}` found.")
+
+    if model_name == "MultipleLinearRegression":
+        model = MultipleLinearRegression()
+
+    return model
