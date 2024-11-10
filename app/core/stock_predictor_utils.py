d04c9693ed3400803c6ca44d67c954e6ac222cfe
Petar <ppaureus@gmail.com>
2024-11-10 15:31:27 +0100

    style

--- app/core/stock_predictor_utils.py
+++ app/core/stock_predictor_utils.py
@@ -2,7 +2,8 @@ from functools import wraps
 from typing import Any, Callable
 import streamlit as st
 
-def title(title: str) -> Callable[...,Any]:
+
+def title(title: str) -> Callable[..., Any]:
     """
     Wraps a function and prints a title before execution.
 
@@ -13,14 +14,18 @@ def title(title: str) -> Callable[...,Any]:
     :return: whathever the wrapped function does.
     :rtype: Callable[...,Any]
     """
-    def decorator(function: Callable[...,Any]) -> Callable[...,Any]:
+
+    def decorator(function: Callable[..., Any]) -> Callable[..., Any]:
         """
         The decorator.
         """
+
         @wraps(function)
         def wrapped_function(*args: Any, **kwargs: Any) -> Any:
             st.write(f"# {title}")
             result = function(*args, **kwargs)
             return result
+
         return wrapped_function
+
     return decorator
